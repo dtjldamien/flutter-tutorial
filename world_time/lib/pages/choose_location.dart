d053bc21e891b815ba1d35c980ebbc7f14e22d35
dtjldamien <dtjldamien@gmail.com>
2020-12-01 14:14:19 +0800

    part 26
    http

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -8,30 +8,30 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
   int counter = 0;
 
-  void getData() async {
-    // simulate network request for a username
-    // await waits for this current statement to finish before running the next line
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'yoshi';
-    });
+  // void getData() async {
+  //   // simulate network request for a username
+  //   // await waits for this current statement to finish before running the next line
+  //   String username = await Future.delayed(Duration(seconds: 3), () {
+  //     return 'yoshi';
+  //   });
 
-    // simulate network request for a bio for the username
-    String bio = await Future.delayed(Duration(seconds: 3), () {
-      return 'musician and egg collector';
-    });
+  //   // simulate network request for a bio for the username
+  //   String bio = await Future.delayed(Duration(seconds: 3), () {
+  //     return 'musician and egg collector';
+  //   });
 
-    // waits for both username and bio to be returned
-    print('$username - $bio');
+  //   // waits for both username and bio to be returned
+  //   print('$username - $bio');
 
-    // async method would not block the other methods from running
-  }
+  //   // async method would not block the other methods from running
+  // }
 
-  @override
-  void initState() {
-    super.initState();
-    print('initState function ran');
-    getData();
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   print('initState function ran');
+  //   getData();
+  // }
 
   @override
   Widget build(BuildContext context) {
